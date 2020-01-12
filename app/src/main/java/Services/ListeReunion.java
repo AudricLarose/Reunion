372720cfbac07ae5a1ce5992a8e6d7df9b55ec55
Larose Audric <l.audric@gmail.com>
2020-01-12 20:31:39 +0100

    voici la version optimitsé de mon application

--- app/src/main/java/Services/ListeReunion.java
+++ app/src/main/java/Services/ListeReunion.java
@@ -9,8 +9,8 @@ import Model.listItem;
 public abstract class ListeReunion {
 
     public static List<listItem> liste_des_items = Arrays.asList(
-            new listItem("Projet 1","19 5, 2019","15:30","George@hotmail.com","Salle 1"),
-            new listItem("Projet 2","Dec 5, 2019","5:30","George@hotmail.com","Salle 2"),
+            new listItem("Projet 1","Dec 5, 2019","15:30","George@hotmail.com","Salle 1"),
+            new listItem("Projet 2","Dec 5, 2019","15:30","George@hotmail.com","Salle 2"),
             new listItem("Projet 3","Dec 8, 2019","14:30","George@hotmail.com","Salle 5"),
             new listItem("Projet 4","Dec 7, 2019","17:30","George@hotmail.com","Salle 5"),
             new listItem("Projet 5","Dec 9, 2019","19:30","George@hotmail.com","Salle 8")
@@ -22,7 +22,7 @@ public abstract class ListeReunion {
 
     public static List<listItem> liste_original = Arrays.asList(
             new listItem("Projet 1","Dec 5, 2019","15:30","George@hotmail.com","Salle 1"),
-            new listItem("Projet 2","Dec 5, 2019","5:30","George@hotmail.com","Salle 2"),
+            new listItem("Projet 2","Dec 5, 2019","15:30","George@hotmail.com","Salle 2"),
             new listItem("Projet 3","Dec 8, 2019","14:30","George@hotmail.com","Salle 5"),
             new listItem("Projet 4","Dec 7, 2019","17:30","George@hotmail.com","Salle 5"),
             new listItem("Projet 5","Dec 9, 2019","19:30","George@hotmail.com","Salle 8")
